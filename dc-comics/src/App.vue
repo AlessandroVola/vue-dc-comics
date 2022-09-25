e29155343f8dc0b337d46c398ebd116464d887bf
AlessandroVola <volaalessandro@gmail.com>
2022-09-25 23:25:00 +0200

    base

---
+++ dc-comics/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>DC Comics</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
+}
+</style>
